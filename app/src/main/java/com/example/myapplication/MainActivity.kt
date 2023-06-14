f0d1c08671905f9a37c32743e6539ea3301536d9
anukisss <ani.kakauridze.1@btu.edu.ge>
2023-06-14 20:27:11 +0400

    "dd"

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button1).setOnClickListener{
+            Toast.makeText(, this, "you clicked on button", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+}
